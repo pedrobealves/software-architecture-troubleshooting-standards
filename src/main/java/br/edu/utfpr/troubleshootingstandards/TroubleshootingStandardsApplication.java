b4951e208392f4e15455abe23466c596549f60f0
PedroTreck <pedrobealves@gmail.com>
2019-05-14 04:13:48 -0300

    Feat: created new pom

--- src/main/java/br/edu/utfpr/troubleshootingstandards/TroubleshootingStandardsApplication.java
+++ src/main/java/br/edu/utfpr/troubleshootingstandards/TroubleshootingStandardsApplication.java
@@ -1,4 +1,4 @@
-package br.edu.utfpr;
+package br.edu.utfpr.troubleshootingstandards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
