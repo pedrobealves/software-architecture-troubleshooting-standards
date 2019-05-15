9f7573611bccee25dd6e8c56c30f66fd426499d4
PedroTreck <pedrobealves@gmail.com>
2019-05-15 18:12:03 -0300

    Issue: test lecturer

--- src/test/java/br/edu/utfpr/troubleshootingstandards/repository/AnticipationRepositoryTest.java
+++ src/test/java/br/edu/utfpr/troubleshootingstandards/repository/AnticipationRepositoryTest.java
@@ -1,28 +1,77 @@
 package br.edu.utfpr.troubleshootingstandards.repository;
 
 import br.edu.utfpr.troubleshootingstandards.model.Anticipation;
+import br.edu.utfpr.troubleshootingstandards.model.Lecturer;
+import br.edu.utfpr.troubleshootingstandards.model.Modalities;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.Optional;
+
+import static br.edu.utfpr.troubleshootingstandards.service.AnticipationServiceTest.CODE;
 import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@TestPropertySource("classpath:application-test.properties")
 public class AnticipationRepositoryTest {
+
+    private static final String REASON = "Gripe";
+    private static final Date DATE = new Date(2019, 5, 14);
+    private static final Date PREVIOUS_DATE = new Date(2019, 5, 28);
+    private static final int NUMBER_CLASSES = 2;
+    private static final boolean IN_PERSON = true;
+    public static final String CODE = "123456";
+    public static final String NAME = "Joe";
 
     @Autowired
     private AnticipationRepository anticipationRepository;
 
+    @Autowired
+    private LecturerRepository lecturerRepository;
+
+    private Lecturer lecturer;
+    private Anticipation anticipation;
+
+    @Before
+    public void setUp(){
+        lecturer = Lecturer
+                .builder()
+                .code(CODE)
+                .name(NAME)
+                .build();
+
+        anticipation = Anticipation
+                .builder()
+                .reason(REASON)
+                .date(DATE)
+                .previousDate(PREVIOUS_DATE)
+                .numberClasses(NUMBER_CLASSES)
+                .modalitie(Modalities.PRESENCIAL)
+                .lecturer(lecturer)
+                .build();
+
+        anticipationRepository.save(anticipation);
+
+    }
+
     @Test
-    public void createShouldPersistData(){
-        Anticipation anticipation = Anticipation.builder().reason("Gripe").build();
-        this.anticipationRepository.save(anticipation);
-        assertThat(anticipation.getReason());
+    public void shouldGetDataLecturer() {
+        Optional<Lecturer> optional = lecturerRepository.findByCode("123456");
+
+        assertThat(optional.isPresent()).isTrue();
+
+        Lecturer lecturer = optional.get();
+        assertThat(lecturer.getCode()).isEqualTo("123456");
+        assertThat(lecturer.getName()).isEqualTo("Joe");
     }
 
 }
--- src/test/java/br/edu/utfpr/troubleshootingstandards/repository/LecturerRepositoryTest.java
+++ src/test/java/br/edu/utfpr/troubleshootingstandards/repository/LecturerRepositoryTest.java
@@ -1,13 +1,53 @@
 package br.edu.utfpr.troubleshootingstandards.repository;
 
+import br.edu.utfpr.troubleshootingstandards.model.Lecturer;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@TestPropertySource("classpath:application-test.properties")
 public class LecturerRepositoryTest {
 
-    @Test
-    public void findByCode() {
+    @Autowired
+    LecturerRepository lecturerRepository;
 
+    @Before
+    public void setUp(){
+        lecturerRepository.save(
+                Lecturer.builder()
+                .code("123456")
+                .name("Joe")
+                .build()
+        );
+    }
+
+    @Test
+    public void shouldGetData() {
+        Optional<Lecturer> optional = lecturerRepository.findByCode("123456");
+
+        assertThat(optional.isPresent()).isTrue();
+
+        Lecturer lecturer = optional.get();
+        assertThat(lecturer.getCode()).isEqualTo("123456");
+        assertThat(lecturer.getName()).isEqualTo("Joe");
+    }
+
+    @Test
+    public void shouldNotGetData() {
+        Optional<Lecturer> optional = lecturerRepository.findByCode("123");
+
+        assertThat(optional.isPresent()).isFalse();
     }
 }
