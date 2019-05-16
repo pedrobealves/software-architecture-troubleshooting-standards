a1bc1a220a8b489690bf67e88b060dd668b889b4
Christian Alves <christian.alvesdasilva@gmail.com>
2019-05-16 09:59:32 -0300

    Subject entity created

---
+++ src/main/java/br/edu/utfpr/troubleshootingstandards/model/Subject.java
@@ -0,0 +1,23 @@
+package br.edu.utfpr.troubleshootingstandards.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Subject implements Serializable {
+    @Id @GeneratedValue
+    private long id;
+    
+    private String code;
+    private String name;
+}
